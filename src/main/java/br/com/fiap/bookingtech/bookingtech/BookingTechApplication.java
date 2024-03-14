347b509ebf7c4b5f2426c0fcc0766a3f0e0735bd
marinautsch <utsch.marina@gmail.com>
2024-03-14 20:42:08 -0300

    Início do repositório com o projeto Spring Data noSQL Booking Tech

---
+++ src/main/java/br/com/fiap/bookingtech/bookingtech/BookingTechApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.bookingtech.bookingtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/bookingtech/bookingtech/BookingTechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.bookingtech.bookingtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
